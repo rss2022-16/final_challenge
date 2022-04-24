a5194f4633897f7e388e915796a31ff82d27f450
Jared Boyer <jaredb@mit.edu>
2022-04-24 17:33:13 -0400

    Blur, find mid lines, average them

--- image_processing/image_processing.py
+++ image_processing/image_processing.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-import scipy
-import math
+import matplotlib.pyplot as plt
 
 height = 376
 width = 672
@@ -9,18 +8,19 @@ mid = int(width/2)
 
 img = cv2.imread('lane.jpg')
 img = cv2.resize(img, (width, height))
-grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.namedWindow("out", cv2.WINDOW_NORMAL)
 
+# Blur
+kernel_size = 7
+blurred = cv2.GaussianBlur(img,(kernel_size, kernel_size),0)
 
-#filter only the white portion of the image and mask everything else
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+# Color Filtering
+hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 lower_white = np.array([0,0,168])
 upper_white = np.array([172,111,255])
 mask = cv2.inRange(hsv, lower_white, upper_white)
-res = cv2.bitwise_and(img, img, mask= mask)
 
-
+# Edge Detection
 canny = cv2.Canny(mask, 50, 200, None, 3)
 color_canny = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
 
@@ -46,11 +46,12 @@ left = linesP[0][0]
 right = linesP[1][0]
 
 #filter out horizontal lines
+slope_thresh = np.pi/6
 vertical_lines = []
 for line in linesP:
     l = line[0]
     angle = np.arctan2(abs(l[3]-l[1]), l[2]-l[0])
-    if(np.pi/6 < angle < np.pi*5/6):
+    if(slope_thresh < angle < np.pi - slope_thresh):
         vertical_lines.append(line)
 
 left = None
@@ -59,28 +60,46 @@ right = None
 for i in range(0, len(vertical_lines)):
     l = vertical_lines[i][0]
     bot_y = max(l[1], l[3])
-    if(l[0] <= mid): 
+    slope = np.arctan2(l[3]-l[1], l[2]-l[0])
+    if min(l[0], l[2])  <= mid: 
         if left is None:
             left = l
-        elif(l[0] > left[0] and bot_y >= max(left[1], left[3])):
+        elif min(l[0], l[2]) > min(left[0], left[2]):
             left = l
     else:
         if right is None:
             right = l
-        if(l[0] < right[0] and bot_y >= max(right[1], right[3])):
+        if max(l[0], l[2]) < max(right[0], right[2]):
             right = l
         
-    cv2.line(color_canny, (l[0], l[1]), (l[2], l[3]), (0,0,255), 2, cv2.LINE_AA)
+    cv2.line(color_canny, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+    # cv2.line(img, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
 
 #mark "closest" two lines in green
-cv2.line(color_canny, (left[0], left[1]), (left[2], left[3]), (0,255,0), 2, cv2.LINE_AA)
-cv2.line(color_canny, (right[0], right[1]), (right[2], right[3]), (0,255,0), 2, cv2.LINE_AA)
+cv2.line(color_canny, (left[0], left[1]), (left[2], left[3]), (0,255,0), 3, cv2.LINE_AA)
+cv2.line(color_canny, (right[0], right[1]), (right[2], right[3]), (0,255,0), 3, cv2.LINE_AA)
+# cv2.line(img, (left[0], left[1]), (left[2], left[3]), (0,255,0), 3, cv2.LINE_AA)
+# cv2.line(img, (right[0], right[1]), (right[2], right[3]), (0,255,0), 3, cv2.LINE_AA)
 
+if left[3] > left[1]: # If endpoint is below start point
+    left[0], left[2] = left[2], left[0]
+    left[1], left[3] = left[3], left[1]
 
+if right[3] > right[1]:
+    right[0], right[2] = right[2], right[0]
+    right[1], right[3] = right[3], right[1]
 
+bot_x = int(left[0]/2 + right[0]/2)
+bot_y = int(left[1]/2 + right[1]/2)
+top_x = int(left[2]/2 + right[2]/2)
+top_y = int(left[3]/2 + right[3]/2)
+
+cv2.line(color_canny, (bot_x, bot_y), (top_x, top_y), (255,0,0), 3, cv2.LINE_AA)
+# cv2.line(img, (bot_x, bot_y), (top_x, top_y), (255,0,0), 3, cv2.LINE_AA)
 
 # cv2.imshow('out', canny)
 # cv2.waitKey(0)
 cv2.imshow('out', color_canny)
+# cv2.imshow('out', img)
 cv2.waitKey(0)
 
